c914641e24f13930c1c045d2c88cc0dc94edc2fa
Wraxuz <boris.egorov@math.msu.ru>
2023-11-10 19:15:21 +0300

    huy

---
+++ number7/task05.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define EPS 1e-14
+#include "array_io.h"
+int solve5 (int k, double* a, int n);
+double maxch(double a, double b);
+int equal(double x, double y);
+int main(int argc, char* argv[]){
+    int n = 0, p = 0, s = 0;
+    int k = 0; 
+    char* name = 0;
+    int task = 5;
+    double* a;
+    double t;
+    int res = 0;
+    if (!((argc == 5 || argc == 6) && sscanf(argv[2], "%d", &n) == 1 && sscanf(argv[3], "%d", &p) == 1 && sscanf(argv[4], "%d", &s) == 1 && sscanf(argv[1], "%d", &k) == 1)){
+        printf("Usage: %s k n p s [name]\n", argv[0]);
+        return 6;
+    }
+    if (argc == 6 && s == 0) name = argv[5];
+    a = (double*)malloc(n * sizeof(double));
+    if (!a){
+        printf("Not enough memory!\n");
+        return 2;
+    }
+    if (name){
+        int res1 = read_array(a, n, name);
+        if (res1 != SUCCESS){
+            switch (res1){
+                case ERROR_OPEN: printf("Cannot open %s;\n", name); break;
+                case ERROR_READ: printf("Cannot read %s\n", name); break;
+                default: printf("Unknown Error %d in file %s;\n", res1, name);
+            }
+            free(a);
+            return 3;
+        }
+    }
+    else init_array(a, n, s);
+    print_array(a, n, p);
+    t = clock();
+    res = solve5(k, a, n);
+    t = (clock() - t) / CLOCKS_PER_SEC;
+    printf ("New array:\n");
+    print_array (a, res, p); 
+    printf ("%s : Task = %d Result = %d Elapsed = %.2f\n",argv[0], task, res, t);
+    free(a);
+    return SUCCESS;
+}
+
+int solve5 (int k, double* a, int n){
+    int p = 0, q = 0, cnt_current = 0, f = 0, f1 =0, cnt_itog = 0;
+    double srar = 0, sum_itog = 0, sum_current = 0;
+    for (int i = 1; i < n; i ++){
+        if (a[i - 1] >= a[i] && f == 0){
+            f = 1;
+            cnt_current += 2;
+            sum_current += a[i - 1] + a[i];
+            f1 = 1;
+        }
+        else if (a[i - 1] >= a[i] && f == 1){
+            cnt_current += 1;
+            sum_current += a[i];
+        }
+        else{
+            if (cnt_current >= k){
+                sum_itog += sum_current;
+                cnt_itog += cnt_current;
+            }
+            sum_current = 0;
+            cnt_current = 0;
+            f = 0;
+        }
+    }
+    if (cnt_current >= k){
+        sum_itog += sum_current;
+        cnt_itog += cnt_current;
+    }
+    if (f1 == 0) return n;
+    srar = sum_itog / cnt_itog;
+    while (p < n){
+        if (a[p] >= srar){
+            a[q] = a[p];
+            q += 1;
+        }
+        p += 1;
+    }
+    return q;
+}
+int equal(double x, double y){
+    if ((fabs (x - y)) < (EPS * (fabs (x) + fabs (y)))){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
---
+++ number7/task06.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define EPS 1e-14
+#include "array_io.h"
+int equal(double x, double y);
+int solve6 (const char* name1, double* a, int n);
+int maybe_insert(double* a, int current_size, int n, double current_number);
+int main(int argc, char* argv[]){
+    int n = 0, p = 0;
+    char* name1;
+    int task = 6;
+    double* a;
+    double t;
+    int res = 0;
+    if (!((argc == 4) && sscanf(argv[1], "%d", &n) == 1 && sscanf(argv[2], "%d", &p) == 1)){
+        printf("Usage: %s name1 n p\n", argv[0]);
+    }
+    name1 = argv[3];
+    a = (double*)malloc(n * sizeof(double));
+    if (!a){
+        printf("Not enough memory!\n");
+        return 2;
+    }
+    t = clock();
+    res = solve6(name1, a, n);
+    if (res < SUCCESS){
+        switch (res){
+            case ERROR_OPEN: printf("Cannot open %s;\n", name1); break;
+            case ERROR_READ: printf("Cannot read %s;\n", name1); break;
+            default: printf("Unknown Error %d in file %s;\n", res, name1);
+        }
+        free(a);
+        return 4;
+    }
+    t = (clock() - t) / CLOCKS_PER_SEC;
+    printf ("New array:\n");
+    print_array (a, res, p); 
+    printf ("%s : Task = %d Result = %d Elapsed = %.2f\n",argv[0], task, res, t);
+    free(a);
+    return SUCCESS;
+}
+int solve6 (const char* name1, double* a, int n){
+    int ans = 0, f = 0;
+    double prev = 0, current_number = 0;
+    FILE* fp;
+    if (n == 0){
+        return 0;
+    }
+    fp = fopen(name1, "r");
+    if (!fp) return ERROR_OPEN;
+    while (fscanf(fp, "%lf", &current_number) == 1){
+        if (f != 0){
+            if (equal(prev, current_number)){
+                ans = maybe_insert(a, ans, n, current_number);
+            }
+        }
+        else{
+            f = 1;
+        }
+        prev = current_number;
+    }
+    if (!feof(fp)){
+        fclose(fp);
+        return ERROR_READ;
+    }
+    fclose(fp);
+    return ans;
+}
+int equal(double x, double y){
+    if ((fabs (x - y)) < (EPS * (fabs (x) + fabs (y)))){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int maybe_insert(double* a, int current_size, int n, double current_number){
+    int k = -1;
+    for (int i = 0; i < n; i ++){
+        if (current_number < a[i]){
+            k = i;
+            break;
+        }
+        if (equal(current_number, a[i])) 
+            return current_size;
+    }
+    if (k == -1){
+        a[current_size] = current_number;
+        current_size += 1;
+        return current_size;
+    }
+    if (current_size == n){
+        double per = 0;
+        for (int i = k; i < current_size; i ++){
+            per = a[i];
+            a[i] = current_number;
+            current_number = per;
+        }
+        return current_size;
+    }
+    else{
+        double per = 0;
+        if (current_size != 0){
+            for (int i = k; i < current_size + 1; i ++){
+                per = a[i];
+                a[i] = current_number;
+                current_number = per;
+            }
+        }
+        else{
+            a[0] = current_number;
+        }
+        current_size += 1;
+        return current_size;
+    }
+}
