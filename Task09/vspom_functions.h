ef12b8e4822469f3b84d712edf8d1bdef6d61000
Wraxuz <boris.egorov@math.msu.ru>
2023-11-29 14:15:27 +0300

    h

--- Task09/vspom_functions.h
+++ Task09/vspom_functions.h
@@ -2,7 +2,12 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 double test_1(size_t n, size_t (*p)(const char *), const char *s, size_t *res);
 double test_2_5 (size_t n, char* (*p) (char*, const char *), char *s1, const char *s2, char **result);
+double test_3_4 (size_t n, char* (*p) (const char *, int), const char *s, int ch, char **res);
 size_t strlen_(const char *);
 char *strcpy_(char *s1, const char *s2);
+char *strchr_ (const char *s, int ch);
+char *strrchr_ (const char *s, int ch);
+char *strcat_ (char *string1, const char *string2);
