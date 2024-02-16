37568929f40b7074f8501e64ffa534af899bff60
Wraxuz <boris.egorov@math.msu.ru>
2024-02-16 20:53:58 +0300

    k

--- 2 семестр/functions.c
+++ 2 семестр/functions.c
@@ -84,7 +84,7 @@ int task3(const char* s, const char* t, char* buf){
         }
         else if (buf[i] == 0) break;
     }
-    for (i = 0; i <= p / 2; i += 2){
+    for (i = 0; i < p; i += 2){
         for (int q = SLOVA[i]; q < SLOVA[i + 1]; q++){
             int maybe_equal = 1;
             if (q != SLOVA[i] && nach){
@@ -119,23 +119,18 @@ int task4(const char* s, const char* t, char* buf){
         return 1;
     for (i = 0; i < LEN; i++){
         if (!strchr(t, buf[i]) && !slovo){
-            printf("%d\n", buf[i]);
             SLOVA[p] = i;
             slovo = 1;
             p ++;
         }
         else if ((strchr(t, buf[i]) || buf[i] == 0) && slovo){
-            printf("%d\n", buf[i]);
             SLOVA[p] = i;
             slovo = 0;
             p ++;
         }
         else if (buf[i] == 0) break;
     }
-    for (i = 0; i <= p / 2; i++)
-        //printf("%d\n", SLOVA[i]);
-    // ---------------------------------------------
-    for (i = 0; i <= p / 2; i += 2){
+    for (i = 0; i < p; i += 2){
         for (int q = SLOVA[i]; q < SLOVA[i + 1]; q++){
             int maybe_equal = 1;
             j = 0;
