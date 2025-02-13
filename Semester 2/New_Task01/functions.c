bfd9514950cfdfa5febd2cdc0d80cc1368a5910f
Wraxuz <boris.egorov@math.msu.ru>
2025-02-14 01:06:37 +0300

    аннигиляторная пушка

--- Semester 2/New_Task01/functions.c
+++ Semester 2/New_Task01/functions.c
@@ -207,3 +207,178 @@ io_status task4(const char* s, const char* t, char* buf, int* r){
     }
     return SUCCESS;
 }
+io_status pattern6(const char* s, char* s1, char* s2){
+    int i = 0, is1 = 0, is2 = 0, prev_back = 0;
+    while (s[i]){
+        if (s[i] == BACKSLASH){
+            if (prev_back){
+                s1[is1] = s[i];
+                s2[is2] = '0';
+                is1 += 1;
+                is2 += 1;
+                prev_back = 0;
+                i += 1;
+                continue;
+            }
+            i += 1;
+            prev_back = 1;
+            if (!s[i]) return ERROR_PATTERN;
+        }
+        else if (s[i] == '.' && !prev_back){
+            s1[is1] = s[i];
+            s2[is2] = '1';
+            is1 += 1;
+            is2 += 1;
+            i += 1;
+        }
+        else{
+            s1[is1] = s[i];
+            s2[is2] = '0';
+            is1 += 1;
+            is2 += 1;
+            i += 1;
+            prev_back = 0;
+        }
+    }
+    s1[is1] = 0;
+    s2[is2] = 0;
+    return SUCCESS;
+}
+
+io_status task6(char* s1, char* s2, char* buf, int* r){
+    int i = 0, maybe_equal = 1;
+    while (s1[i]){
+        if (s2[i] == '1' && buf[i]){
+            i += 1;
+        }
+        else{
+            if (s1[i] == buf[i]){
+                i += 1;
+            }
+            else{
+                maybe_equal = 0;
+                break;
+            }
+        }
+    }
+    if (maybe_equal && !buf[i]){
+        *r += 1;
+        return SUCCESS;
+    }
+    return SUCCESS;
+}
+
+io_status pattern8(const char* s, char*s3, int* s4, char* s5){
+    char s1[LEN];
+    char s2[LEN];
+    int i = 0, is1 = 0, is2 = 0, is3 = 0, is4 = 0, is5 = 0, special = 0, gruppa = 0, cnt = 0, prev_back = 0;
+    while (s[i]){
+        if (s[i] == BACKSLASH){
+            if (prev_back){
+                s1[is1] = s[i];
+                s2[is2] = '0';
+                is1 += 1;
+                is2 += 1;
+                prev_back = 0;
+                i += 1;
+                continue;
+            }
+            i += 1;
+            prev_back = 1;
+            if (!s[i]) return ERROR_PATTERN;
+        }
+        else if (s[i] == '+' && !prev_back){
+            if (i == 0){
+                s1[is1] = s[i];
+                s2[is2] = '0';
+                is1 += 1;
+                is2 += 1;
+            }
+            else{
+                s2[is2 - 1] = '1';
+            }
+            i += 1;
+        }
+        else{
+            s1[is1] = s[i];
+            s2[is2] = '0';
+            is1 += 1;
+            is2 += 1;
+            i += 1;
+            prev_back = 0;
+        }
+    }
+    s1[is1] = 0;
+    s2[is2] = 0;
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    for (i = 0; i < is1 - 1; i++){
+        if (s1[i] == s1[i + 1] && !gruppa){
+            s3[is3] = s1[i];
+            special = ((s2[i] == '1') || (s2[i + 1] == '1'));
+            gruppa = 1;
+            cnt += 2;
+        }
+        else if (s1[i] == s1[i + 1] && gruppa){
+            special = (special || (s2[i + 1] == '1'));
+            cnt += 1;
+        }
+        else if (gruppa){
+            is3 += 1;
+            s4[is4] = cnt;
+            is4 += 1;
+            s5[is5] = special ? '1' : '0';
+            special = 0;
+            is5 += 1;
+            cnt = 0;
+            gruppa = 0;
+        }
+        else{
+            s3[is3] = s1[i];
+            is3 += 1;
+            s4[is4] = 1;
+            is4 += 1;
+            s5[is5] = (s2[i] == '1') ? '1' : '0';
+            is5 += 1;
+        }
+    }
+    if (gruppa){
+            is3 += 1;
+            s4[is4] = cnt;
+            is4 += 1;
+            s5[is5] = special ? '1' : '0';
+            special = 0;
+            is5 += 1;
+            cnt = 0;
+            gruppa = 0;
+        }
+    else{
+            s3[is3] = s1[i];
+            is3 += 1;
+            s4[is4] = 1;
+            is4 += 1;
+            s5[is5] = (s2[i] == '1') ? '1' : '0';
+            is5 += 1;
+    }
+    s3[is3] = 0;
+    s4[is4] = 0;
+    s5[is5] = 0;
+    return SUCCESS;
+}
+
+io_status task8(char* s3, int* s4, char* s5, char* buf, int* r){
+    int i = 0, cnt = 0;
+    int j = 0;
+    cnt = 0;
+    for(i = 0; s3[i]; i++){
+        cnt = 0;
+        while (buf[j + cnt] == s3[i]){
+            cnt += 1;
+        }
+        if (s5[i] == '0' && cnt != s4[i]) return SUCCESS;
+        if (s5[i] == '1' && cnt < s4[i]) return SUCCESS;
+        j += cnt;
+    }
+    *r += 1;
+    return SUCCESS;
+}
--- Semester 2/New_Task01/vspom_functions.h
+++ Semester 2/New_Task01/vspom_functions.h
@@ -10,13 +10,14 @@ typedef enum io_status_ {
     ERROR_READ,
     ERROR_PATTERN,
 } io_status;
-//#define SUCCESS 0
-//#define ERROR_OPEN -1
-//#define ERROR_READ -2
-//#define ERROR_LAST_BACKSLASH -3
+
 io_status task1(const char* s, char* buf, int* r);
 io_status pattern2 (const char* s, char* s1, int* s2);
 io_status task2(const char* s, char* buf, char* s1, int s2, int* r);
 io_status task3(const char* s, const char* t, char* buf, int* r);
 io_status task4(const char* s, const char* t, char* buf, int* r);
+io_status pattern6(const char* s, char* s1, char* s2);
+io_status task6(char* s1, char* s2, char* buf, int* r);
+io_status pattern8(const char* s, char*s3, int* s4, char* s5);
+io_status task8(char* s3, int* s4, char* s5, char* buf, int* r);
 
