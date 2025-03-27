a486ea210345d763211ef070a6212ba77ec5e2a0
Wraxuz <boris.egorov@math.msu.ru>
2025-03-27 11:40:48 +0300

    1 Co-authored-by: interstate57 <interstate57@users.noreply.github.com>

--- Semester 2/Task08/solve.c
+++ Semester 2/Task08/solve.c
@@ -1,19 +1,29 @@
 #include <math.h>
 #include "solve.h"
-
+#define EPS 1e-14
+ 
 
 double solve1(double* a, double* x, double* xm, int n, int m){
-    /*деление на 0*/
     int i;
+    xm = x;
+    vectcpy(xm, x, n);
     for (i = 1; i <= m; i ++){
+        vectcpy(x, xm, n);
         proisv_matr_na_vect(a, x, xm, n);
     }
-    return skal_proisv(xm, x, n) / skal_proisv(x, x, n);
+    if (equal(skal_proisv(x, x, n), 0)){
+        return 1e308;
+    }
+    else{
+        return skal_proisv(xm, x, n) / skal_proisv(x, x, n);
+    }
 }
 
 void solve2(double* a, double* x, double* b, double* xm, int n, int m, double tau){
     int i;
+    vectcpy(xm, x, n);
     for (i = 1; i <= m; i++){
+        vectcpy(x, xm, n);
         proisv_matr_na_vect(a, x, xm, n);
         razn_vect(b, xm, xm, n);
         umnozh_vect_na_ch(xm, xm, n, tau);
@@ -96,5 +106,22 @@ void umnozh_vect_na_ch(double* a, double* b, int n, double tau){
     int i;
     for (i = 0; i < n; i++){
         b[i] = a[i] * tau;
+    }
+}
+
+void vectcpy(double* a, double* b, int n){
+    int i;
+    double vr = 0;
+    for (i = 0; i < n; i++){
+        a[i] = b[i];
+    }
+}
+
+int equal(double x, double y){
+    if ((fabs (x - y)) < (EPS  * (fabs (x) + fabs (y)))){
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
--- Semester 2/Task08/solve.h
+++ Semester 2/Task08/solve.h
@@ -14,4 +14,7 @@ int solve10(double* a, int m, int n);
 double r2_1(double* a, double* xm, int n, double r1);
 void proisv_matr_na_vect(double* a, double* b, double* c, int n);
 double skal_proisv(double* a, double* b, int n);
-void proisv_matr_na_vect(double* a, double* b, double* c, int n);
+double r1(double* a, double* b, double* xm, int n);
+double r2(double* xm, int n);
+void razn_vect(double* a, double* b, double* c, int n);
+void umnozh_vect_na_ch(double* a, double* b, int n, double tau);
