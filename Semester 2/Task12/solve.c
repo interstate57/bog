78413372954f6af10b104b94a534b0c3e90c285b
Wraxuz <boris.egorov@math.msu.ru>
2025-05-06 23:47:36 +0300

    7

--- Semester 2/Task12/solve.c
+++ Semester 2/Task12/solve.c
@@ -46,16 +46,16 @@ double solve5(double (*f) (double), double a, double b, int n){
 
 double solve6(double (*f) (double), double a, double b, int n){
     double h = (b - a) / n;
-    double sum = 2 * f(b) * (2/3 * pow(fabs(a + h * (n - 1)), 3/2) + pow(fabs(b), 3/2) / 3 -\
+    double sum = 2 * f(b) * (2/3 * pow(fabs(a + h * (n - 1)), 3./2) + pow(fabs(b), 3./2) / 3 -\
         (a + h * (n - 1)) * sgn(b) * sqrt(fabs(b)));
     double sub_sum = 0;
     double xi = a;
     int i;
-    sum += 2 * f(a) * (2/3 * pow(fabs(a + h), 3/2) + pow(fabs(a), 3/2) / 3 -\
+    sum += 2 * f(a) * (2/3 * pow(fabs(a + h), 3./2) + pow(fabs(a), 3./2) / 3 -\
         (a + h) * sgn(a) * sqrt(fabs(a)));
     for (i = 1; i < n; i++){
         xi += h;
-        sub_sum += f(xi) * (2/3 * (pow(fabs(xi - h), 3/2) + pow(fabs(xi), 3/2) + pow(fabs(xi + h), 3/2)) - sgn(xi) * sqrt(fabs(xi)) * 2 * xi);
+        sub_sum += f(xi) * (2/3 * (pow(fabs(xi - h), 3./2) + pow(fabs(xi), 3./2) + pow(fabs(xi + h), 3./2)) - sgn(xi) * sqrt(fabs(xi)) * 2 * xi);
     }
     sum += 2 * sub_sum;
     return sum;
@@ -67,35 +67,37 @@ double solve7(double (*f) (double), double a, double b, int n){
     double suma = 0, suma1 = 0, sumb1 = 0, sumb = 0;
     double xi = a + h;
     int i;
-    suma = f(a) * ((pow(fabs(a + 2*h), 5/2) * sgn(a + 2*h) - pow(fabs(a), 5/2) * sgn(a)) / (5 * pow(h, 2)) +\
+    suma = f(a) * ((pow(fabs(a + 2*h), 5./2) * sgn(a + 2*h) - pow(fabs(a), 5./2) * sgn(a)) / (5 * pow(h, 2)) +\
         (sqrt(fabs(a + 2*h)) * sgn(a + 2*h) - sqrt(fabs(a)) * sgn(a)) * (2 + 3 * a / h) -\
-        (pow(fabs(a + 2*h), 3/2) - pow(fabs(a), 3/2)) * (1/h + 2 / (3 * pow(h, 2))));
-    suma1 = f(a + h) * (-2 / (5 * pow(h, 2)) * (pow(fabs(a + 2*h), 5/2) * sgn(a + 2*h) - pow(fabs(a), 5/2) * sgn(a)) +\
-        (1 / (5 * pow(h, 2))) * (pow(fabs(a + 3*h), 5/2) * sgn(a + 3*h) - pow(fabs(a + h), 5/2) * sgn(a + h))-\
+        (pow(fabs(a + 2*h), 3./2) - pow(fabs(a), 3./2)) * (1/h + 2 / (3 * pow(h, 2))));
+    suma1 = f(a + h) * (-2 / (5 * pow(h, 2)) * (pow(fabs(a + 2*h), 5./2) * sgn(a + 2*h) - pow(fabs(a), 5./2) * sgn(a)) +\
+        (1 / (5 * pow(h, 2))) * (pow(fabs(a + 3*h), 5./2) * sgn(a + 3*h) - pow(fabs(a + h), 5./2) * sgn(a + h))-\
         (4 * a / h) * (sqrt(fabs(a + 2*h)) * sgn(a + 2*h) - sqrt(fabs(a)) * sgn(a)) +\
         (sqrt(fabs(a + 3*h)) * sgn(a + 3*h) - sqrt(fabs(a + h)) * sgn(a + h)) * (2 + 3 * (a + h) / h) +\
-        (pow(fabs(a + 2*h), 3/2) - pow(fabs(a), 3/2)) * (4 / (3 * h) + 4 * a / (3 * pow(h, 2))) -\
-        (pow(fabs(a + 3*h), 3/2) - pow(fabs(a + h), 3/2)) * (1 / h - 2 / (3 * pow(h, 2))));
-    sumb1 = f(b - h) * ((1 / (5 * pow(h, 2))) * (pow(fabs(b - h), 5/2) * sgn(b - h) - pow(fabs(b - 3*h), 5/2) * sgn(b - 3*h)) -\
-        2 / (5 * pow(h, 2)) * (pow(fabs(b), 5/2) * sgn(b) - pow(fabs(b - 2*h), 5/2) * sgn(b - 2*h)) +\
+        (pow(fabs(a + 2*h), 3./2) - pow(fabs(a), 3./2)) * (4 / (3 * h) + 4 * a / (3 * pow(h, 2))) -\
+        (pow(fabs(a + 3*h), 3./2) - pow(fabs(a + h), 3./2)) * (1 / h - 2 / (3 * pow(h, 2))));
+    sumb1 = f(b - h) * ((1 / (5 * pow(h, 2))) * (pow(fabs(b - h), 5./2) * sgn(b - h) - pow(fabs(b - 3*h), 5./2) * sgn(b - 3*h)) -\
+        2 / (5 * pow(h, 2)) * (pow(fabs(b), 5./2) * sgn(b) - pow(fabs(b - 2*h), 5./2) * sgn(b - 2*h)) +\
         (b - 3*h) / h * (sqrt(fabs(b - h)) * sgn(b - h) - sqrt(fabs(b - 3*h)) * sgn(b - 3*h)) -\
         4 * (b - 2*h) / h * (sqrt(fabs(b)) * sgn(b) - sqrt(fabs(b - 2*h)) * sgn(b - 2*h)) -\
-        (pow(fabs(b - h), 3/2) - pow(fabs(b - 3*h), 3/2)) * (1 / (3 * h) - 2 * (b - 3*h) / (3 * pow(h, 2))) +\
-        (pow(fabs(b), 3/2) - pow(fabs(b - 2*h), 3/2)) * (4 / (3 * h) + 4 * (b - 2*h) / (3 * pow(h, 2))));
-    sumb = f(b) * ((1 / (5 * pow(h, 2))) * (pow(fabs(b), 5/2) * sgn(b) - pow(fabs(b - 2*h), 5/2) * sgn(b - 2*h)) +\
+        (pow(fabs(b - h), 3./2) - pow(fabs(b - 3*h), 3./2)) * (1 / (3 * h) - 2 * (b - 3*h) / (3 * pow(h, 2))) +\
+        (pow(fabs(b), 3./2) - pow(fabs(b - 2*h), 3./2)) * (4 / (3 * h) + 4 * (b - 2*h) / (3 * pow(h, 2))));
+    sumb = f(b) * ((1 / (5 * pow(h, 2))) * (pow(fabs(b), 5./2) * sgn(b) - pow(fabs(b - 2*h), 5./2) * sgn(b - 2*h)) +\
         ((b - 2*h) / h) * (sqrt(fabs(b)) * sgn(b) - sqrt(fabs(b - 2*h)) * sgn(b - 2*h)) -\
-        (pow(fabs(b), 3/2) - pow(fabs(b - 2*h), 3/2)) * (1 / (3 * h) + 2 * (b - 2*h) / (3 * pow(h, 2))));
+        (pow(fabs(b), 3./2) - pow(fabs(b - 2*h), 3./2)) * (1 / (3 * h) + 2 * (b - 2*h) / (3 * pow(h, 2))));
+        count += 4;
     for (i = 2; i < n - 1; i++){
         xi += h;
-        sum += f(xi) * ((1 / (5 * pow(h, 2))) * (pow(fabs(xi), 5/2) * sgn(xi) - pow(fabs(xi - 2*h), 5/2) * sgn(xi - 2*h)) -\
-        2 / (5 * pow(h, 2)) * (pow(fabs(xi + h), 5/2) * sgn(xi + h) - pow(fabs(xi - h), 5/2) * sgn(xi - h)) +\
-        1 / (5 * pow(h, 2)) * (pow(fabs(xi + 2*h), 5/2) * sgn(xi + 2*h) - pow(fabs(xi), 5/2) * sgn(xi)) +\
+        sum += f(xi) * ((1. / (5 * pow(h, 2))) * (pow(fabs(xi), 5./2) * sgn(xi) - pow(fabs(xi - 2*h), 5./2) * sgn(xi - 2*h)) -\
+        2 / (5 * pow(h, 2)) * (pow(fabs(xi + h), 5./2) * sgn(xi + h) - pow(fabs(xi - h), 5./2) * sgn(xi - h)) +\
+        1 / (5 * pow(h, 2)) * (pow(fabs(xi + 2*h), 5./2) * sgn(xi + 2*h) - pow(fabs(xi), 5./2) * sgn(xi)) +\
         (xi - 2*h) / h * (sqrt(fabs(xi)) * sgn(xi) - sqrt(fabs(xi - 2*h)) * sgn(xi - 2*h)) -\
         4 * (xi - h) / h) * (sqrt(fabs(xi + h)) * sgn(xi + h) - sqrt(fabs(xi - h)) * sgn(xi - h) +\
         (sqrt(fabs(xi + 2*h)) * sgn(xi + 2*h) - sqrt(fabs(xi)) * sgn(xi)) * (2 + 3 * xi / h) -\
-        (pow(fabs(xi), 3/2) - pow(fabs(xi - 2*h), 3/2)) * (1 / (3 * h) - 2 * (xi - 2*h) / 3 * pow(h, 2)) +\
-        (pow(fabs(xi + h), 3/2) - pow(fabs(xi - h), 3/2)) * (4 / (3 * h) + 4 * (xi - h) / (3 * pow(h, 2))) -\
-        (pow(fabs(xi + 2*h), 3/2) - pow(fabs(xi), 3/2)) * (1 / h - 2 / (3 * pow(h, 2))));
+        (pow(fabs(xi), 3./2) - pow(fabs(xi - 2*h), 3./2)) * (1 / (3 * h) - 2 * (xi - 2*h) / (3 * pow(h, 2))) +\
+        (pow(fabs(xi + h), 3./2) - pow(fabs(xi - h), 3./2)) * (4 / (3 * h) + 4 * (xi - h) / (3 * pow(h, 2))) -\
+        (pow(fabs(xi + 2*h), 3./2) - pow(fabs(xi), 3./2)) * (1 / h - 2 / (3 * pow(h, 2))));
+        count += 1;
     }
     sum += suma + suma1 + sumb1 + sumb;
     return sum;
@@ -164,12 +166,10 @@ int solve9(double (*f) (double), double a, double b, double epsilon, double* r){
             break;
         }
         for(i = 0; a + (2*i + 1)*h < b; i++){
-            //printf("3\n");
             s2n2 += h * f(a + (2*i + 1)*h);
             count += 1;
         }
         s2n2 = 2./3 * s2n2;
-        //printf("444444444444444444444444444444444444444444444444444444444\n");
         s2n1 = (sn1 + sn2) * 0.5;
         s2n = s2n1 + s2n2 * 2;
         printf("sn = %le, s2n = %le\n", sn, s2n);
@@ -193,6 +193,26 @@ double solve10(double (*f) (double), double a, double epsilon, double* r){
         int k;
         b = a + h;
         k = solve8(f, a, b, epsilon, &I);
+        if (k < 0) break;
+        s += I;
+        if (fabs(I) < epsilon){
+            *r = s;
+            return b;
+        }
+        a = b;
+        h *= 2;
+    }
+    return -1;
+}
+
+double solve11(double (*f) (double), double a, double epsilon, double* r){
+    double s = 0, h = 1;
+    double b;
+    while (1){
+        double I;
+        int k;
+        b = a + h;
+        k = solve9(f, a, b, epsilon, &I);
         if (k < 0) break;
         s += I;
         if (fabs(I) < epsilon){
