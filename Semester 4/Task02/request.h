a27d8bef3af5a46a0b1cff5641208c0bcd495104
Wraxuz <boris.egorov@math.msu.ru>
2026-03-02 01:05:34 +0300

    hfjdkdx

--- Semester 4/Task02/request.h
+++ Semester 4/Task02/request.h
@@ -367,7 +367,6 @@ class request_6{
             return spaces[(unsigned int)symbol] == 1;
         }
         io_status parse_s(char* s){
-            printf("** xyu\n");
             int inside_word = 0;
             int prev_back = 0;
             int i = 0;
@@ -376,7 +375,7 @@ class request_6{
                 //printf("");
                 if (is_space(s[j])) {
                     if (prev_back) {
-                        return io_status::parsing_error;
+                        prev_back = 0;
                     }
                     if (inside_word) {
                         inside_word = 0;
@@ -408,7 +407,6 @@ class request_6{
 
         
         int context_fit(char* str){
-            printf("Processing %s\n", str);
             int i, j;
             int l;
             int len;
@@ -454,62 +452,174 @@ class request_6{
             }
             return 0;
         }
+};
 
-        io_status parse_s_old(char* s, char* t){
-            int i = 0;
-            int j = 0;
-            int prodolzh_slova = 0;
+class pair{
+    private:
+        char a = 0;
+        char b = 0;
+    public:
+        pair() = default;
+        pair(char x, char y){
+            a = x;
+            b = y;
+        }
+        ~pair() = default;
+
+        bool letter_in_between(char x){
+            if (x >= a && x <= b)
+                return true;
+            return false;
+        }
+        char get_a(){
+            return a;
+        }
+        char get_b(){
+            return b;
+        }
+
+};
+
+class request_8{
+    private:
+        char spaces[256] = {};
+        char dop[LEN] = {};
+        int special_symbols[LEN] = {};
+        int begining_of_words[LEN] = {};
+        pair check[LEN] = {};
+        int cnt_words = 0;
+    public:
+        request_8() = default;
+        ~request_8() = default;
+        void parse_t(const char* t){
+            int i;
+            for (i = 0; t[i]; ++i) { 
+                spaces[(unsigned int)t[i]] = 1;
+            }
+        }
+        int is_space(char symbol) {
+            return spaces[(unsigned int)symbol] == 1;
+        }
+        io_status parse_s(char* s){
+            int inside_word = 0;
             int prev_back = 0;
-            for (j = 0; s[j] && spaces[(unsigned int)s[j]] == 1; j++);
-            prodolzh_slova = 1;
-            begining_of_words[0] = 0;
-            cnt_words += 1;
-            for (; s[j];j++){
-                if (spaces[(unsigned int)s[j]] == 1){
-                    if (prodolzh_slova == 1){
-                        prodolzh_slova = 0;
+            int i = 0;
+
+            for (int j = 0; s[j]; j++){
+                //printf("");
+                if (is_space(s[j])) {
+                    if (prev_back) {
+                        prev_back = 0;
                     }
-                    prev_back = 0;
+                    if (inside_word) {
+                        inside_word = 0;
+                    }
                     continue;
                 }
-                else if (s[j] == BACKSLASH){
-                    if (prev_back){
-                        dop[i] = s[j];
-                        i += 1;
-                        prev_back = 0;
-                    } 
-                    else{
-                        prev_back = 1;
-                        if (prodolzh_slova == 0){
-                            begining_of_words[cnt_words] = i;
-                            cnt_words += 1;
-                            prodolzh_slova = 1;
-                        }
-                        if (!s[j + 1] || spaces[(unsigned int)s[j + 1]] == 1)
-                            return io_status::parsing_error;
-                    }
+                
+                if (s[j] == BACKSLASH && !prev_back) {
+                    prev_back = 1;
+                    continue;
                 }
-                else if (s[j] == '_'){
-                    dop[i] = s[j];
-                    if (!prev_back)
-                        special_symbols[i] = 1;
-                    i += 1;
-                    prev_back = 0;
 
+                if (!inside_word) {
+                    begining_of_words[cnt_words] = i;
+                    inside_word = 1;
+                    cnt_words += 1;
                 }
-                else{
-                    if (prodolzh_slova == 0){
-                        begining_of_words[cnt_words] = i;
-                        cnt_words += 1;
-                        prodolzh_slova = 1;
+                if (s[j] == '[' && !prev_back){
+                    int k = 1;
+                    char a = 0;
+                    char b = 0;
+                    int zakr = 0;
+                    for (k; s[j + k]; k++){
+                        if (k == 1){
+                            a = s[j + k];
+                            continue;
+                        }
+                        if (k == 2){
+                            if (s[j + k] != '-')
+                                return io_status::parsing_error;
+                            continue;
+                        }
+                        if (k == 3){
+                            b = s[j + k];
+                            continue;
+                        }
+                        if (k == 4){
+                            zakr = s[j + k] == ']' ? 1 : 0;
+                            break;
+                        }
                     }
+                    if (!zakr && k != 4)
+                        return io_status::parsing_error;
                     dop[i] = s[j];
-                    i += 1;
+                    special_symbols[i] = 1;
+                    check[i] = pair(a, b);
+                    j += k;
+                    i++;
                     prev_back = 0;
                 }
+                dop[i] = s[j];
+                i++;
+                prev_back = 0;
             }
             begining_of_words[cnt_words] = i;
-            begining_of_words[cnt_words + 1] = -1;
+            if (prev_back)
+                return io_status::parsing_error;
             return io_status::success;
         }
+
+        
+        int context_fit(char* str){
+            int i, j;
+            int l;
+            int len;
+            /*
+            printf("begining: ");
+            for (i = 0; i <= cnt_words; ++i){
+                printf("%d ", begining_of_words[i]);
+            }
+            printf("\n");
+            printf("dop: " );
+            for (i = 0; dop[i];){
+                printf("%c ", dop[i]);
+                i++;
+            }
+            printf("\n");
+            */
+            for (j = 0; str[j] && is_space(str[j]); j++);
+            for (; str[j];) {
+                for (i = 0; str[j + i] && !is_space(str[j + i]); ++i);
+                len = i;
+                for (int k = 0; k < cnt_words; k++){
+                    bool error = false;
+                    if (begining_of_words[k + 1] - begining_of_words[k] != len)
+                        continue;
+                    int nach_slova = begining_of_words[k];
+                    for (l = 0; l < len; l++){
+                        if (special_symbols[nach_slova + l])
+                            if (!check[nach_slova + l].letter_in_between(str[j + l])){
+                                error = true;
+                                break;
+                            }
+                        else{
+                            if (dop[nach_slova + l] == str[j + l])
+                                continue;
+                            else{
+                                error = true;
+                                break;
+                            }
+                        }
+                    }
+                    if ((l == len) && !error)
+                        return 1;
+                }
+                j += len;
+                for (; str[j] && spaces[(unsigned int)str[j]] == 1; j++);
+            }
+            return 0;
+        }
+
+
 };
--- Semester 4/Task02/solve.h
+++ Semester 4/Task02/solve.h
@@ -210,3 +210,45 @@ io_status solve6(char* a, char* b, char* s, char* t, int* r){
     fclose(in);
     return io_status::success;
 }
+
+io_status solve8(char* a, char* b, char* s, char* t, int* r){
+    FILE* in;
+    FILE* out;
+    if (!(in = fopen(a, "r"))){
+        return io_status::open_error_a;
+    }
+    if (!(out = fopen(b, "w"))){
+        fclose(in);
+        return io_status::open_error_b;
+    }
+    request_8 request;
+    request.parse_t(t);
+    io_status ret = request.parse_s(s);
+    if (ret != io_status::success){
+        fclose(in);
+        fclose(out);
+        return ret;
+    }
+    char buf[LEN];
+    int cnt = 0;
+    while(fgets(buf, LEN, in) != 0) {
+		for(int i = 0; buf[i]; ++i) {
+			if(buf[i] == '\n') {
+				buf[i] = 0; 
+				break;
+			}
+		}
+		if (request.context_fit(buf) == 1) {
+			fprintf(out, "%s\n", buf);
+			cnt += 1;
+		}
+	}
+    *r = cnt;
+    fclose(out);
+    if (!feof(in)){
+        fclose(in);
+        return io_status::eof;
+    }
+    fclose(in);
+    return io_status::success;
+}
